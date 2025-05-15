536a67c1561680cf2a17eaec2adcca659e988f51
Luis <luisangelhc2004@hotmail.com>
2025-05-14 18:41:21 -0600

    Se soluciona lista ubicacion imagenes

--- app/src/main/java/com/example/mobile1project/thirdpartial/Location/LocationListScreen.kt
+++ app/src/main/java/com/example/mobile1project/thirdpartial/Location/LocationListScreen.kt
@@ -49,12 +49,12 @@ fun LocationListScreen(viewModel: LocationViewModel = viewModel()) {
                         verticalAlignment = Alignment.CenterVertically,
                         modifier = Modifier.padding(8.dp)
                     ) {
-                        /*Image(
-                            painter = painterResource(id = getDrawableId(location.imageUrl)),
+                        Image(
+                            painter = painterResource(id = getDrawableId(location.imageUrl.lowercase())),
                             contentDescription = null,
                             modifier = Modifier.size(64.dp),
                             contentScale = ContentScale.Crop
-                        )*/
+                        )
                         Spacer(modifier = Modifier.width(8.dp))
                         Column {
                             Text(text = "Name: ${location.name}")
